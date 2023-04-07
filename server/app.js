1d09686fb5c6b1387165481347901ca24d97e55b
Samuel Sau <samuel51@usf.edu>
2023-04-07 17:20:42 -0400

    connected to postgresql on google cloud, created user model, updated package.json

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+const db = require('./config/db');
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -12,7 +12,10 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 // Import your routes and use them here
 
+//Database connection
+db.testConnection();
+
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
---
+++ server/config/db.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+const pool = new Pool({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT
+});
+
+const testConnection = async () => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    console.log('Connected to PostgreSQL:', result.rows[0].now);
+  } catch (error) {
+    console.error('Error connecting to PostgreSQL:', error.message);
+  }
+};
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  testConnection,
+};
+
+  
